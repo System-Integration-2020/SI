5a720f303154c008e6316d39224977da73fec02f
maleneH <themalenehansen@gmail.com>
2020-11-20 13:46:05 +0100

    .

--- P5-RMI-DB-Server/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
--- This file has fixed name and location
-DROP TABLE IF EXISTS Customer;
-
-CREATE TABLE Customer (
-  accnum INTEGER PRIMARY KEY,
-  name VARCHAR(250) NOT NULL,
-  amount DOUBLE
-);
